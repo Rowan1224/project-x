9459a734e3cfbd4d0ca6d7f2231599eccd0be630
KhanShaheb34 <shakirul34@student.sust.edu>
2020-06-14 00:49:09 +0600

    Dummy api call test

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import APITest from "../components/home/apiTest";
 
 const Home = (props) => {
   return (
     <div>
       <Link to="/about">About Us</Link>
       <h1>Welcome to project-x</h1>
+      <APITest />
     </div>
   );
 };
