e4063fe75c1c19bef46ff6157a0502cad56654f4
KhanShaheb34 <shakirul34@student.sust.edu>
2020-06-19 22:20:35 +0600

    Search Bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/home";
-import About from "./pages/about";
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
           <Route path="/">
             <Home />
           </Route>
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Layout from "../components/generic/layout";
+import LocationSearch from "../components/home/locationSearch";
 
 const Home = (props) => {
   return (
     <Layout>
-      <Link to="/about">About Us</Link>
-      <h1 style={{ height: 1233 }}>Welcome to project-x</h1>
+      <LocationSearch />
     </Layout>
   );
 };
