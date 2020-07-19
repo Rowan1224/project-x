feac7d1ff0c3e84be101539d2ccea526717e8793
The Philosopher <shahriarelahi3062@gmail.com>
2020-07-19 23:02:12 +0600

    card style changed

--- client/src/components/home/service providers/providers.jsx
+++ client/src/components/home/service providers/providers.jsx
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap';
 import Provider from './provider';
 import { v4 as uuidv4 } from 'uuid';
 import { LocationContext } from '../../../contexts/LocationContext';
+import emoji from 'react-easy-emoji'
 
 const Providers = () => {
   const services = [
@@ -35,7 +36,9 @@ const Providers = () => {
   ) : (
     <Container>
       <div className='text-center mt-5 pt-5'>
-        <h2>Eita re veritcally center kore dis SHAKIRUL</h2>
+        <h2 className='bg-light border p-3 my-5'>
+          Select a location to see the service providers { emoji("😀") }
+        </h2>
       </div>
     </Container>
   );
--- client/src/components/home/service providers/services.jsx
+++ client/src/components/home/service providers/services.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Image } from 'react-bootstrap';
 import Service from './service';
 import { v4 as uuidv4 } from 'uuid';
+import emoji from 'react-easy-emoji'
 
 const Services = () => {
   const services = [
@@ -24,10 +25,17 @@ const Services = () => {
   return (
     <Container>
       <div className='mt-5 pt-1 text-center'>
-        <Image className='w-100' src='/img/profile_pic.jpg' />
-        <h2 className='mt-5'>
-          Best dry cleaning service you can get so use our service already -_-
+        <Image className='w-100 border border-secondary' src='/img/profile_pic.jpg' />
+        {/* <Image
+          className='w-100'
+          src={`https://picsum.photos/id/${Math.floor(
+            Math.random() * 1000
+          )}/800`}
+        /> */}
+        <h2 className='bg-light border p-3 my-5'>
+          Best dry cleaning service for the money { emoji("🤪") }
         </h2>
+        <h3 className='pt-3'>Our Services</h3>
       </div>
       <div className='row mt-5'>
         {services.map((name) => (
