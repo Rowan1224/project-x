d8403c0a548070e121d7eb422356499955970d6a
The Philosopher <shahriarelahi3062@gmail.com>
2020-08-14 01:07:33 +0600

    Shopping cart functionality added

--- client/src/components/home/service providers/services.jsx
+++ client/src/components/home/service providers/services.jsx
@@ -1,15 +1,19 @@
 import React, { useContext, useState, useEffect } from "react";
-import { Image } from "react-bootstrap";
+import { Image, Modal, Button } from "react-bootstrap";
 import Service from "./service";
 import { v4 as uuidv4 } from "uuid";
 import emoji from "react-easy-emoji";
 import Infobar from "../../generic/infobar";
 import { ThemeContext } from "../../../contexts/ThemeContext";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Services = (props) => {
     const [services, setServices] = useState([]);
     const [sName, setSName] = useState("");
+    const [show, setShow] = useState(false);
     const params = useParams();
 
     // componentDidMount
@@ -39,10 +43,11 @@ const Services = (props) => {
 
     useEffect(() => {
         const API_URL = "/getProfile/";
-         
+
         const loadData = async () => {
             const servideID = {
-                service_id: services.length > 0 ? services[0].service_id : undefined,
+                service_id:
+                    services.length > 0 ? services[0].service_id : undefined,
             };
 
             const response = await fetch(API_URL, {
@@ -63,10 +68,69 @@ const Services = (props) => {
 
     // Themes
     const { isLightTheme, theme } = useContext(ThemeContext);
+    const ui = isLightTheme ? theme.light.ui : theme.dark.ui;
     const border = isLightTheme ? theme.light.border : theme.dark.border;
+    const syntax = isLightTheme ? theme.light.syntax : theme.dark.syntax;
+    const type = isLightTheme ? theme.light.type : theme.dark.type;
+    const currency_text = isLightTheme
+        ? theme.light.currency_text
+        : theme.dark.currency_text;
+
+    const handleShow = () => {
+        setShow(true);
+    };
+
+    const handleClose = () => {
+        setShow(false);
+    };
+
+    // const handleTimeout = () => {
+    //     setTimeout(handleClose, 1500);
+    // };
 
     return (
         <div>
+            <Modal
+                size="md"
+                centered
+                show={show}
+                onHide={handleClose}
+                // onEntered={handleTimeout}
+            >
+                <div className={"rounded" + ui + border + syntax}>
+                    <Modal.Header>
+                        <Modal.Title className={"mx-auto" + currency_text}>
+                            Item added
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className="text-center">
+                        This item has been added to your cart
+                        <h3 className={"mt-4" + currency_text}>
+                            <FontAwesomeIcon
+                                className="fa-icon"
+                                icon={["fas", "check"]}
+                            />
+                        </h3>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button
+                            variant={type}
+                            onClick={handleClose}
+                            // className="mb-4"
+                        >
+                            Close
+                        </Button>
+                        <Button
+                            variant={"outline-" + type}
+                            as={Link}
+                            to="/cart"
+                        >
+                            Show cart
+                        </Button>
+                    </Modal.Footer>
+                </div>
+            </Modal>
+
             <div className="mt-5 pt-1 text-center">
                 <div className="inner">
                     <Image
@@ -83,13 +147,18 @@ const Services = (props) => {
                 </div>
 
                 <Infobar>
-                    {sName ? sName + " " : "Company name"}{emoji("🤪")}
+                    {sName ? sName + " " : "Company name"}
+                    {emoji("🤪")}
                 </Infobar>
                 <h3 className="pt-5">Our Services</h3>
             </div>
             <div className="row mt-5">
                 {services.map((service) => (
-                    <Service serviceInfo={service} key={uuidv4()} />
+                    <Service
+                        serviceInfo={service}
+                        key={uuidv4()}
+                        handleShow={handleShow}
+                    />
                 ))}
             </div>
         </div>
