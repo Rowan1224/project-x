a11f354d0265b7abff1b6a35fd883ea61addd768
Henrick993 <tohasadat993@gmail.com>
2021-02-25 01:40:57 +0600

    Updated query

--- server/controllers/employee.js
+++ server/controllers/employee.js
@@ -1,25 +1,33 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../util/database');
-const Employee = require('../models/Employee');
+const Sequelize = require("sequelize");
+const sequelize = require("../util/database");
+const Employee = require("../models/Employee");
 const employee = Employee(sequelize, Sequelize);
 
 exports.getEmployee = (req, res, next) => {
     const service_id = req.body.service_id;
-    
-    employee.findAll({
-        attributes: ['employee_id', 'service_id', 'employee_name', 'phone_number'],
-        where: {
-            service_id: service_id
-        }
-    }).then((emp) => {
-        res.status(200).json({
-            employee: emp,
-            message: "Success"
+
+    employee
+        .findAll({
+            attributes: [
+                "employee_id",
+                "service_id",
+                "employee_name",
+                "phone_number",
+            ],
+            where: {
+                service_id: service_id,
+            },
+        })
+        .then((emp) => {
+            res.status(200).json({
+                employee: emp,
+                message: "Success",
+            });
+        })
+        .catch({
+            employee: "NaN",
+            message: "Failed",
         });
-    }).catch({
-        employee: "NaN",
-        message: "Failed"
-    });
 };
 
 exports.addEmployee = (req, res, next) => {
@@ -27,46 +35,72 @@ exports.addEmployee = (req, res, next) => {
     const employee_name = req.body.employee_name;
     const phone_number = req.body.phone_number;
 
-    employee.create({
-        service_id: service_id,
-        employee_name: employee_name,
-        phone_number: phone_number
-    }).then((result) => {
-        res.status(200).json({message: "Success"});
-    }).catch((err) => {
-        res.status(504).json({message: "Failed"});
-    });
+    employee
+        .findAll({
+            where: {
+                phone_number: phone_number
+            },
+        })
+        .then((ret) => {
+            if (ret.length > 0) {
+                res.status(504).json({
+                    message: "Already an employee with that number",
+                });
+            } else {
+                employee
+                    .create({
+                        service_id: service_id,
+                        employee_name: employee_name,
+                        phone_number: phone_number,
+                    })
+                    .then((result) => {
+                        res.status(200).json({ message: "Successfully added." });
+                    })
+                    .catch((err) => {
+                        res.status(504).json({ message: "Failed to add." });
+                    });
+            }
+        });
+
 };
 
 exports.updateEmployee = (req, res, next) => {
     const employee_id = req.body.employee_id;
     let updated_employee_name = req.body.employee_name;
-    let  updated_phone_number = req.body.phone_number ;
+    let updated_phone_number = req.body.phone_number;
 
-    employee.findByPk(employee_id).then(
-        emp => {
-            emp.employee_name = updated_employee_name ? updated_employee_name : emp.employee_name ;
-            emp.phone_number = updated_phone_number? updated_phone_number : emp.phone_number ;
+    employee
+        .findByPk(employee_id)
+        .then((emp) => {
+            emp.employee_name = updated_employee_name
+                ? updated_employee_name
+                : emp.employee_name;
+            emp.phone_number = updated_phone_number
+                ? updated_phone_number
+                : emp.phone_number;
 
             return emp.save();
-        }
-    ).then((result) => {
-        res.status(200).json({message: "Success.Update Completed"});
-    }).catch((err) => {
-        res.status(504).json({message: "Failed to Update."});
-    });
+        })
+        .then((result) => {
+            res.status(200).json({ message: "Success.Update Completed" });
+        })
+        .catch((err) => {
+            res.status(504).json({ message: "Failed to Update." });
+        });
 };
 
 exports.deleteEmployee = (req, res, next) => {
-    const employee_id = req.body.employee_id
+    const employee_id = req.body.employee_id;
 
-    employee.findByPk(employee_id).then(
-        (emp) => {
+    employee
+        .findByPk(employee_id)
+        .then((emp) => {
             return emp.destroy();
-        }
-    ).then((result) => {
-        res.status(200).json({message: "Success"});
-    }).catch((err) => {
-        res.status(504).json({message: "Failed"});
-    });
+        })
+        .then((result) => {
+            res.status(200).json({ message: "Success" });
+        })
+        .catch((err) => {
+            res.status(504).json({ message: "Failed" });
+        });
 };
