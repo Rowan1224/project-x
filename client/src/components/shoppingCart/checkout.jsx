90386097456beabb2159d81382e2d36613088eae
Henrick993 <tohasadat993@gmail.com>
2021-04-16 17:05:22 +0600

    moment time fix test 5.1

--- client/src/components/shoppingCart/checkout.jsx
+++ client/src/components/shoppingCart/checkout.jsx
@@ -111,7 +111,8 @@ const Checkout = (props) => {
 
                     var currentTime = new Date();
                     const convertTime = moment(currentTime).tz("Asia/Dhaka").format("YYYY-MM-DD HH:mm:ss");
-                    const today = new Date(convertTime);
+                    const today = new Date(convertTime); 
+                    // nothing here
                     const date =
                         today.getFullYear() +
                         "-" +
